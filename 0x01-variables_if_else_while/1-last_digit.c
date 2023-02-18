747fe057f1e022f65276ef1cab3f845e07244ecd
Neo Sekhukhune <neotron1625@gmail.com>
2023-02-18 10:38:36 -0800

    Task 1 improved

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 		printf("Last digit of %d is 0 and is 0\n", n);
 	} else
 	{
-		printf("Last digit of %d is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
 	return (0);
 }
