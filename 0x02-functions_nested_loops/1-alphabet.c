a5b3d7dbbd46f10c9976ff73312a9f52df713871
Neo Sekhukhune <neotron1625@gmail.com>
2023-02-21 16:34:40 -0800

    First attempt at tasl 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#incude "main.h"
+
+/**
+ * print_alphabet - Entry
+ * Return: Always 0
+ */
+void print_alphabet(void);
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
