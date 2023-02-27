51414c8f357008f5903a3fa9c38d730d7c898de7
Neo Sekhukhune <neotron1625@gmail.com>
2023-02-27 15:29:26 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Functions that prints a string
+ * @str: Input String
+ */
+void _puts(char *str);
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*str);
+		i++;
+	}
+}
