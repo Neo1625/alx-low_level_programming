d8f5cf13ed940d65c684c11b0dcc26c6def6cdfe
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-02 02:22:21 -0800

    First attempt at task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - Function that concatenates two strings
+ * @dest: String input
+ * @src: String input
+ * Return:newly concatenated dest
+ */
+char *_strcat(char *dest, char *src);
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	int src_len = 0;
+
+	while (src[src_len] != '\0')
+	{
+		dest[dest_len++] = src[src_len++];
+	}
+	dest[dest_len++] = '\0';
+
+	return (dest);
+}
+
