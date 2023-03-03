cab7ec11504131bd3f0556be3eb11dcc65545240
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-02 18:44:23 -0800

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase
+ * to uppercase.
+ * @s: String input.
+ *
+ * Return: uppercase string
+ */
+char *string_toupper(char *s)
+{
+	int s_len = 0;
+
+	while (s[s_len] != '\0')
+	{
+		if (s[s_len] >= 65 && s[s_len] <= 90)
+			continue
+		else
+			s[s_len] = s[s_len] - 32;
+		s_len++;
+	}
+
+	return (s);
+}
