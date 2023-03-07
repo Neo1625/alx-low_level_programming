86b3f5adac0c40e8fc5f0c224d7da1d84743c4d1
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-06 18:27:14 -0800

    TASK 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk
+ * @s: String input
+ * @accept: String containing the characters to match
+ *
+ * Return: Pointer to the byte in s.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+
+		if (accept[j])
+			return (s + i);
+	}
+
+	return (0);
+}
