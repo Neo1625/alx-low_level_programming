27ed0df260a10df091e13f51260724011940f9b9
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-08 09:39:35 -0800

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,13 @@
  * _puts_recursion - Function thats prints a string
  * @s: String input.
  *
- * Return: 0
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
 	if (s[0] == '\0')
 		_putchar('\n');
-		return (0);
+		return;
 	_putchar(s[0]);
 	_puts_recursion(s + 1);
 }
