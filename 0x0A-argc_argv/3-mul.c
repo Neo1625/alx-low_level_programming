bd1c37bc1606b150f17b1ba555af07c7d99f6884
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-12 09:22:53 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry
+ * @argc: Number of arguments.
+ * @argv: Vector of arguments.
+ *
+ * Return: 0. if the two multiplicants are OR 1 otherwise.
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	int first;
+	int second;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	first = atoi(argv[1]);
+
+	second = atoi(argv[2]);
+
+	result = first * second;
+
+	printf("%d\n", result);
+
+	return (0);
+}
