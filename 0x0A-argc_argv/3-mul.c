2083afca8e9cb7d607d60369812ba0318697f43f
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-12 14:07:11 -0700

    TAsk 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,25 +13,17 @@ int main(int argc, char *argv[])
 {
 	int result;
 
-	int first;
-	int second;
-
-	if (argc <= 1)
+	if (argc != 3)
 	{
 		printf("Error\n");
-
 		return (1);
 	}
 	else
 	{
-		first = atoi(argv[1]);
-
-		second = atoi(argv[2]);
-
-		result = first * second;
-
+		result = (atoi(argv[1]) * atoi(argv[2]));
 		printf("%d\n", result);
 
 		return (0);
 	}
 }
+
