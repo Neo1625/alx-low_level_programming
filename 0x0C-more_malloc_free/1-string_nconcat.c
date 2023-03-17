b35089f88fe64b4c964c11033e83f675e05f99c2
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-16 17:22:17 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,8 +11,8 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, m;
 	char *str;
+	unsigned int i, j, m;
 
 	if (s1 == NULL)
 	{
@@ -23,6 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (i = 0; s1[i]; i++)
 			;
 	}
+
 	if (s2 == NULL)
 	{
 		j = 0;
@@ -32,24 +33,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (j = 0; s2[j]; j++)
 			;
 	}
+
 	if (j > n)
 	{
 		j = n;
 	}
 	str = malloc(sizeof(char) * (i + j + 1));
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
+
 	for (m = 0; m < i; m++)
 	{
 		str[m] = s1[m];
 	}
+
 	for (m = 0; m < j; m++)
 	{
 		str[m + i] = s2[m];
 	}
-	str[i + j] = '\0';
 
+	str[i + j] = '\0';
 	return (str);
 }
