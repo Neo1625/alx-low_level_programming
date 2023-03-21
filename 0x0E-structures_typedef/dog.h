9d5ed92cfbd3af8facf61f91b4d0f4fecb95762c
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-20 19:13:51 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct my_dog - My dog struct
+ * @name: String input.
+ * @age: Integer input.
+ * @owner: String input.
+ */
+struct my_dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
