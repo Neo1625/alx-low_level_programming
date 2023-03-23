c4fd1518dc08bfda58f5c1bcb2f4563641041494
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-23 01:36:40 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Function that prints a name
+ * @name: Name to print
+ * @f: Function pointer
+ *
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
