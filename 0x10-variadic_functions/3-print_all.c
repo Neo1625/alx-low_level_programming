b94110d4031cc75d1c5c76958d2af119079b4a32
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-25 05:29:07 -0700

    TASK 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,59 +2,102 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 
+void _printf(va_list f);
+void _printi(va_list i);
+void _printc(va_list c);
+void _prints(va_list s);
+void print_all(const char * const format, ...);
+
+/**
+ * _printf - Function that prints float
+ * @f: Float to print.
+ *
+ * Return: void.
+ */
+void _printf(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * _printi - Function that prints integer
+ * @i: Integer to print.
+ *
+ * Return: void.
+ */
+void _printi(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * _printc - Function thagt prints a char.
+ * @c: Character to print
+ *
+ * Return: void.
+ */
+void _printc(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * _prints - Functions that prints a string.
+ * @s: String input.
+ *
+ * Return: void.
+ */
+void _prints(va_list s)
+{
+	char *str;
+
+	str = va_arg(s, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+
 /**
  * print_all - Function that prints anything.
- * @format: List of types of arguments passed to the function.
+ * @format: List of types of arguments passed to the function
  *
  * Return: void.
  */
 void print_all(const char * const format, ...)
 {
-	va_list arg;
-	char c;
-	int i;
-	float f;
-	char *s;
+	dt_t types[] = {
+		{"c", _printc},
+		{"i", _printi},
+		{"f", _printf},
+		{"s", _prints},
+		{NULL, NULL}
+	};
 
-	const char *p = format;
+	int i, j = 0;
+	va_list a_list;
+	char *separator = "";
 
-	va_start(arg, format);
+	va_start(a_list, format);
 
-	while (*p != '\0')
+	while (format != NULL && format[j] != '\0')
 	{
-		switch (*p)
+		i = 0;
+		while (types[i].type != NULL)
 		{
-			case 'c':
-				c = va_arg(arg, int);
-				printf("%c", c);
-				break;
-			case 'i':
-				i = va_arg(arg, int);
-				printf("%d", i);
-				break;
-			case 'f':
-				f = va_arg(arg, double);
-				printf("%f", f);
-				break;
-			case 's':
-				s = va_arg(arg, char *);
-				if (s == NULL)
-				{
-					printf("(nil)");
-				}
-				else
-				{
-					printf("%s", s);
-				}
-				break;
+			if (*(types[i].type) == format[j])
+			{
+				printf("%s", separator);
+				(types[i].func)(a_list);
+				separator = ", ";
+			}
+			i++;
 		}
-		++p;
-		if (*p != '\0')
-		{
-			if (*p == 'c' || *p == 'i' || *p == 'f' || *p == 's')
-				printf(", ");
-		}
+		j++;
 	}
 	printf("\n");
-	va_end(arg);
+	va_end(a_list);
 }
