4a4a34b0dd646907c9a75f3f7c63551eca9ad772
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-30 15:59:41 -0700

    TASK 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,11 +16,11 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str != NULL)
 		{
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[0] (nil)\n");
 		}
 		nodes++;
 	}
