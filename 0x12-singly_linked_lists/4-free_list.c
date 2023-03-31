8dc8668c469547234951e8f07c6b4f7565642b45
Neo Sekhukhune <neotron1625@gmail.com>
2023-03-30 17:41:57 -0700

    TASK 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Function that frees a list_t list.
+ *
+ * @head: head of singly linked list.
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	free(head);
+}
